d16ca7133ad0acc373443f2f4901b9d005295159
neurotempest <jdc.tas@gmail.com>
2023-03-02 17:44:52 +0000

    tmpl: Add FuncWithContextAndError method

--- tmpl/declarations.go
+++ tmpl/declarations.go
@@ -4,6 +4,52 @@ import (
 	"github.com/thecodedproject/gopkg"
 )
 
+// FuncWithContextAndError returns a gopkg.DeclFunc with the given name, args
+// and returnArgs, but with additionally taking a `context.Context` as the
+// first argument and return an `error` as the last return argument.
+//
+// The `context.Context` argument will always have the name `ctx` and the `error`
+// return argument will be either unnamed or named `err`, depending on whether
+// returnArgs contains unnamed or named `gopkg.DeclVar`s
+func FuncWithContextAndError(
+	funcName string,
+	args []gopkg.DeclVar,
+	returnArgs []gopkg.DeclVar,
+) gopkg.DeclFunc {
+
+	f := gopkg.DeclFunc{
+		Name: funcName,
+	}
+
+	f.Args = append(
+		[]gopkg.DeclVar{
+			{
+				Name: "ctx",
+				Type: gopkg.TypeUnknownNamed{
+					Name: "Context",
+					Import: "context",
+				},
+			},
+		},
+		args...,
+	)
+
+	errVarName := ""
+	if len(returnArgs) > 0 && returnArgs[0].Name != "" {
+		errVarName = "err"
+	}
+
+	f.ReturnArgs = append(
+		returnArgs,
+		gopkg.DeclVar{
+			Name: errVarName,
+			Type: gopkg.TypeError{},
+		},
+	)
+
+	return f
+}
+
 func UnnamedReturnArgs(retArgs ...gopkg.Type) []gopkg.DeclVar {
 
 	ret := make([]gopkg.DeclVar, 0, len(retArgs))
--- tmpl/declarations_test.go
+++ tmpl/declarations_test.go
@@ -9,6 +9,136 @@ import (
 	"github.com/thecodedproject/gopkg/tmpl"
 )
 
+func TestFuncWithContextAndError(t *testing.T) {
+
+	testCases := []struct{
+		Name string
+		FuncName string
+		Args []gopkg.DeclVar
+		RetArgs []gopkg.DeclVar
+		Expected gopkg.DeclFunc
+	}{
+		{
+			Name: "empty returns func with ctx and unnamed err",
+			Expected: gopkg.DeclFunc{
+				Args: []gopkg.DeclVar{
+					{
+						Name: "ctx",
+						Type: gopkg.TypeUnknownNamed{
+							Name: "Context",
+							Import: "context",
+						},
+					},
+				},
+				ReturnArgs: []gopkg.DeclVar{
+					{
+						Type: gopkg.TypeError{},
+					},
+				},
+			},
+		},
+		{
+			Name: "with some args and unnamed return args",
+			FuncName: "MyMethod",
+			Args: []gopkg.DeclVar{
+				{
+					Name: "a",
+					Type: gopkg.TypeInt{},
+				},
+				{
+					Name: "b",
+					Type: gopkg.TypeString{},
+				},
+			},
+			RetArgs: []gopkg.DeclVar{
+				{
+					Type: gopkg.TypeFloat32{},
+				},
+				{
+					Type: gopkg.TypeBool{},
+				},
+			},
+			Expected: gopkg.DeclFunc{
+				Name: "MyMethod",
+				Args: []gopkg.DeclVar{
+					{
+						Name: "ctx",
+						Type: gopkg.TypeUnknownNamed{
+							Name: "Context",
+							Import: "context",
+						},
+					},
+					{
+						Name: "a",
+						Type: gopkg.TypeInt{},
+					},
+					{
+						Name: "b",
+						Type: gopkg.TypeString{},
+					},
+				},
+				ReturnArgs: []gopkg.DeclVar{
+					{
+						Type: gopkg.TypeFloat32{},
+					},
+					{
+						Type: gopkg.TypeBool{},
+					},
+					{
+						Type: gopkg.TypeError{},
+					},
+				},
+			},
+		},
+		{
+			Name: "with named return args",
+			FuncName: "MyMethod",
+			RetArgs: []gopkg.DeclVar{
+				{
+					Name: "a",
+					Type: gopkg.TypeFloat32{},
+				},
+			},
+			Expected: gopkg.DeclFunc{
+				Name: "MyMethod",
+				Args: []gopkg.DeclVar{
+					{
+						Name: "ctx",
+						Type: gopkg.TypeUnknownNamed{
+							Name: "Context",
+							Import: "context",
+						},
+					},
+				},
+				ReturnArgs: []gopkg.DeclVar{
+					{
+						Name: "a",
+						Type: gopkg.TypeFloat32{},
+					},
+					{
+						Name: "err",
+						Type: gopkg.TypeError{},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.Name, func(t *testing.T) {
+			require.Equal(
+				t,
+				test.Expected,
+				tmpl.FuncWithContextAndError(
+					test.FuncName,
+					test.Args,
+					test.RetArgs,
+				),
+			)
+		})
+	}
+}
+
 func TestUnnamedReturnArgs(t *testing.T) {
 
 	testCases := []struct{
